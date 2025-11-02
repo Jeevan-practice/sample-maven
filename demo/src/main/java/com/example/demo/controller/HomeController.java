0613546764f96f96c92229bd6b29c2186b85cd07
root <root@ip-172-31-44-103.us-east-2.compute.internal>
2025-11-02 06:24:21 +0000

    cors entry

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.example.demo.controller;
  
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
  
+@CrossOrigin(origins = "http://deploy-frontend-3101.s3-website.us-east-2.amazonaws.com/") // <-- This fixes CORS error
 @RestController
 public class HomeController {
  
     @GetMapping("/")
     public String home() {
-        return "✅ Spring Boot WAR is deployed successfully!";
+        return "Welcome to Spring Boot WAR Deployment on EC2!";
+    }
+ 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from EC2 Spring Boot App!";
     }
 }
  
